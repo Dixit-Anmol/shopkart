718982dcbdd3076eb5df4e8ec0c2d1e7be9a4d88
Dixit-Anmol <bholudixitanmoldixit@gmail.com>
2025-04-10 09:54:32 +0530

    initial commit

---
+++ src/main/java/org/example/shopkart/ShopkartApplication.java
@@ -0,0 +1,13 @@
+package org.example.shopkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopkartApplication.class, args);
+    }
+
+}
